b737f29063de06a8151b48e8e42d333934f6bb54
Leonardo Salazar <leozxxz@gmail.com>
2021-12-14 19:38:18 -0500

    test: Added a general test to know if the app can initialize

---
+++ modules/kitchen/tests/Feature/AppTest.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+/**
+ * @covers \App\Console\Kernel
+ * @covers \App\Exceptions\Handler
+ * @covers \App\Http\Kernel
+ * @covers \App\Providers\AppServiceProvider
+ * @covers \App\Providers\RouteServiceProvider
+ *
+ * @internal
+ */
+class AppTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testAppInitialize(): void
+    {
+        $response = $this->get('/');
+
+        $response->assertNotFound();
+    }
+}
