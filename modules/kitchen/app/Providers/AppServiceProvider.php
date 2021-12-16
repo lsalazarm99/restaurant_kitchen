bc03004ff1378c68a5b20b9c5a0a971adf8e9535
Leonardo Salazar <leozxxz@gmail.com>
2021-12-16 13:20:30 -0500

    fix: Fixed the URL scheme inside the service

--- modules/kitchen/app/Providers/AppServiceProvider.php
+++ modules/kitchen/app/Providers/AppServiceProvider.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Pagination\AbstractPaginator;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Str;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -17,6 +18,7 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
+        URL::forceScheme(Str::contains(config('app.url'), 'https://') ? 'https' : null);
         URL::forceRootUrl(config('app.url'));
         AbstractPaginator::currentPathResolver(static fn () => app('url')->current());
         JsonResource::withoutWrapping();
