74fbda055be3afd9d509d044d730dc3af965293e
Leonardo Salazar <leozxxz@gmail.com>
2021-12-16 13:15:26 -0500

    fix: Fixed the URL inside the service

--- modules/kitchen/app/Providers/AppServiceProvider.php
+++ modules/kitchen/app/Providers/AppServiceProvider.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace App\Providers;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Pagination\AbstractPaginator;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -15,6 +17,8 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
+        URL::forceRootUrl(config('app.url'));
+        AbstractPaginator::currentPathResolver(static fn () => app('url')->current());
         JsonResource::withoutWrapping();
     }
 }
