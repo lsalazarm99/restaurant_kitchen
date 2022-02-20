02fbe467873e1d817f38fb17b4f05ea9d3b950d5
Leonardo Salazar <leozxxz@gmail.com>
2022-02-20 03:48:56 -0500

    revert: Return the correct HTTP status code and headers
    I don't remember why I did this, but clearly I didn't run the tests
    because it was breaking most of them.
    
    Laravel already returns a message, so I don't need this.

--- modules/kitchen/app/Exceptions/Handler.php
+++ modules/kitchen/app/Exceptions/Handler.php
@@ -5,14 +5,7 @@ declare(strict_types=1);
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
-use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class Handler extends ExceptionHandler
 {
-    public function register(): void
-    {
-        $this->renderable(function (HttpException $exception) {
-            return response()->json(['message' => $exception->getMessage()]);
-        });
-    }
 }
